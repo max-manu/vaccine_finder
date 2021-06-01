c21a99e7a8fa37af8ba8f709452313c72e317c91
max-manu <78288336+max-manu@users.noreply.github.com>
2021-06-01 17:52:24 +0530

    click RUN or Type `python3 simple_run.py` after copying this file
    This file is made with idea `get start fast`
    Simply copy this file store it with any name you like example: simple_run.py
    Then Run this file like normal python file
    `python3 simple_run.py`  must be written without quotes in terminal

---
+++ simple_run.py
@@ -0,0 +1,174 @@
+dic={"andaman and nicobar islands": {"nicobar": 3, "north and middle andaman": 1, "south andaman": 2}, "andhra pradesh": {"anantapur": 9, "chittoor": 10, "east godavari": 11, "guntur": 5, "krishna": 4, "kurnool": 7, "prakasam": 12, "sri potti sriramulu nellore": 13, "srikakulam": 14, "visakhapatnam": 8, "vizianagaram": 15, "west godavari": 16, "ysr district, kadapa (cuddapah)": 6}, "arunachal pradesh": {"anjaw": 22, "changlang": 20, "dibang valley": 25, "east kameng": 23, "east siang": 42, "itanagar capital complex": 17, "kamle": 24, "kra daadi": 27, "kurung kumey": 21, "lepa rada": 33, "lohit": 29, "longding": 40, "lower dibang valley": 31, "lower siang": 18, "lower subansiri": 32, "namsai": 36, "pakke kessang": 19, "papum pare": 39, "shi yomi": 35, "siang": 37, "tawang": 30, "tirap": 26, "upper siang": 34, "upper subansiri": 41, "west kameng": 28, "west siang": 38}, "assam": {"baksa": 46, "barpeta": 47, "biswanath": 765, "bongaigaon": 57, "cachar": 66, "charaideo": 766, "chirang": 58, "darrang": 48, "dhemaji": 62, "dhubri": 59, "dibrugarh": 43, "dima hasao": 67, "goalpara": 60, "golaghat": 53, "hailakandi": 68, "hojai": 764, "jorhat": 54, "kamrup metropolitan": 49, "kamrup rural": 50, "karbi-anglong": 51, "karimganj": 69, "kokrajhar": 61, "lakhimpur": 63, "majuli": 767, "morigaon": 55, "nagaon": 56, "nalbari": 52, "sivasagar": 44, "sonitpur": 64, "south salmara mankachar": 768, "tinsukia": 45, "udalguri": 65, "west karbi anglong": 769}, "bihar": {"araria": 74, "arwal": 78, "aurangabad": 77, "banka": 83, "begusarai": 98, "bhagalpur": 82, "bhojpur": 99, "buxar": 100, "darbhanga": 94, "east champaran": 105, "gaya": 79, "gopalganj": 104, "jamui": 107, "jehanabad": 91, "kaimur": 80, "katihar": 75, "khagaria": 101, "kishanganj": 76, "lakhisarai": 84, "madhepura": 70, "madhubani": 95, "munger": 85, "muzaffarpur": 86, "nalanda": 90, "nawada": 92, "patna": 97, "purnia": 73, "rohtas": 81, "saharsa": 71, "samastipur": 96, "saran": 102, "sheikhpura": 93, "sheohar": 87, "sitamarhi": 88, "siwan": 103, "supaul": 72, "vaishali": 89, "west champaran": 106}, "chandigarh": {"chandigarh": 108}, "chhattisgarh": {"balod": 110, "baloda bazar": 111, "balrampur": 112, "bastar": 113, "bemetara": 114, "bijapur": 115, "bilaspur": 116, "dantewada": 117, "dhamtari": 118, "durg": 119, "gariaband": 120, "gaurela pendra marwahi ": 136, "janjgir-champa": 121, "jashpur": 122, "kanker": 123, "kawardha": 135, "kondagaon": 124, "korba": 125, "koriya": 126, "mahasamund": 127, "mungeli": 128, "narayanpur": 129, "raigarh": 130, "raipur": 109, "rajnandgaon": 131, "sukma": 132, "surajpur": 133, "surguja": 134}, "dadra and nagar haveli": {"dadra and nagar haveli": 137}, "daman and diu": {"daman": 138, "diu": 139}, "delhi": {"central delhi": 141, "east delhi": 145, "new delhi": 140, "north delhi": 146, "north east delhi": 147, "north west delhi": 143, "shahdara": 148, "south delhi": 149, "south east delhi": 144, "south west delhi": 150, "west delhi": 142}, "goa": {"north goa": 151, "south goa": 152}, "gujarat": {"ahmedabad": 154, "ahmedabad corporation": 770, "amreli": 174, "anand": 179, "aravalli": 158, "banaskantha": 159, "bharuch": 180, "bhavnagar": 175, "bhavnagar corporation": 771, "botad": 176, "chhotaudepur": 181, "dahod": 182, "dang": 163, "devbhumi dwaraka": 168, "gandhinagar": 153, "gandhinagar corporation": 772, "gir somnath": 177, "jamnagar": 169, "jamnagar corporation": 773, "junagadh": 178, "junagadh corporation": 774, "kheda": 156, "kutch": 170, "mahisagar": 183, "mehsana": 160, "morbi": 171, "narmada": 184, "navsari": 164, "panchmahal": 185, "patan": 161, "porbandar": 172, "rajkot": 173, "rajkot corporation": 775, "sabarkantha": 162, "surat": 165, "surat corporation": 776, "surendranagar": 157, "tapi": 166, "vadodara": 155, "vadodara corporation": 777, "valsad": 167}, "haryana": {"ambala": 193, "bhiwani": 200, "charkhi dadri": 201, "faridabad": 199, "fatehabad": 196, "gurgaon": 188, "hisar": 191, "jhajjar": 189, "jind": 204, "kaithal": 190, "karnal": 203, "kurukshetra": 186, "mahendragarh": 206, "nuh": 205, "palwal": 207, "panchkula": 187, "panipat": 195, "rewari": 202, "rohtak": 192, "sirsa": 194, "sonipat": 198, "yamunanagar": 197}, "himachal pradesh": {"bilaspur": 219, "chamba": 214, "hamirpur": 217, "kangra": 213, "kinnaur": 216, "kullu": 211, "lahaul spiti": 210, "mandi": 215, "shimla": 208, "sirmaur": 212, "solan": 209, "una": 218}, "jammu and kashmir": {"anantnag": 224, "bandipore": 223, "baramulla": 225, "budgam": 229, "doda": 232, "ganderbal": 228, "jammu": 230, "kathua": 234, "kishtwar": 231, "kulgam": 221, "kupwara": 226, "poonch": 238, "pulwama": 227, "rajouri": 237, "ramban": 235, "reasi": 239, "samba": 236, "shopian": 222, "srinagar": 220, "udhampur": 233}, "jharkhand": {"bokaro": 242, "chatra": 245, "deoghar": 253, "dhanbad": 257, "dumka": 258, "east singhbhum": 247, "garhwa": 243, "giridih": 256, "godda": 262, "gumla": 251, "hazaribagh": 255, "jamtara": 259, "khunti": 252, "koderma": 241, "latehar": 244, "lohardaga": 250, "pakur": 261, "palamu": 246, "ramgarh": 254, "ranchi": 240, "sahebganj": 260, "seraikela kharsawan": 248, "simdega": 249, "west singhbhum": 263}, "karnataka": {"bagalkot": 270, "bangalore rural": 276, "bangalore urban": 265, "bbmp": 294, "belgaum": 264, "bellary": 274, "bidar": 272, "chamarajanagar": 271, "chikamagalur": 273, "chikkaballapur": 291, "chitradurga": 268, "dakshina kannada": 269, "davanagere": 275, "dharwad": 278, "gadag": 280, "gulbarga": 267, "hassan": 289, "haveri": 279, "kodagu": 283, "kolar": 277, "koppal": 282, "mandya": 290, "mysore": 266, "raichur": 284, "ramanagara": 292, "shimoga": 287, "tumkur": 288, "udupi": 286, "uttar kannada": 281, "vijayapura": 293, "yadgir": 285}, "kerala": {"alappuzha": 301, "ernakulam": 307, "idukki": 306, "kannur": 297, "kasaragod": 295, "kollam": 298, "kottayam": 304, "kozhikode": 305, "malappuram": 302, "palakkad": 308, "pathanamthitta": 300, "thiruvananthapuram": 296, "thrissur": 303, "wayanad": 299}, "ladakh": {"kargil": 309, "leh": 310}, "lakshadweep": {"agatti island": 796, "lakshadweep": 311}, "madhya pradesh": {"agar": 320, "alirajpur": 357, "anuppur": 334, "ashoknagar": 354, "balaghat": 338, "barwani": 343, "betul": 362, "bhind": 351, "bhopal": 312, "burhanpur": 342, "chhatarpur": 328, "chhindwara": 337, "damoh": 327, "datia": 350, "dewas": 324, "dhar": 341, "dindori": 336, "guna": 348, "gwalior": 313, "harda": 361, "hoshangabad": 360, "indore": 314, "jabalpur": 315, "jhabua": 340, "katni": 353, "khandwa": 339, "khargone": 344, "mandla": 335, "mandsaur": 319, "morena": 347, "narsinghpur": 352, "neemuch": 323, "panna": 326, "raisen": 359, "rajgarh": 358, "ratlam": 322, "rewa": 316, "sagar": 317, "satna": 333, "sehore": 356, "seoni": 349, "shahdol": 332, "shajapur": 321, "sheopur": 346, "shivpuri": 345, "sidhi": 331, "singrauli": 330, "tikamgarh": 325, "ujjain": 318, "umaria": 329, "vidisha": 355}, "maharashtra": {"ahmednagar": 391, "akola": 364, "amravati": 366, "aurangabad ": 397, "beed": 384, "bhandara": 370, "buldhana": 367, "chandrapur": 380, "dhule": 388, "gadchiroli": 379, "gondia": 378, "hingoli": 386, "jalgaon": 390, "jalna": 396, "kolhapur": 371, "latur": 383, "mumbai": 395, "nagpur": 365, "nanded": 382, "nandurbar": 387, "nashik": 389, "osmanabad": 381, "palghar": 394, "parbhani": 385, "pune": 363, "raigad": 393, "ratnagiri": 372, "sangli": 373, "satara": 376, "sindhudurg": 374, "solapur": 375, "thane": 392, "wardha": 377, "washim": 369, "yavatmal": 368}, "manipur": {"bishnupur": 398, "chandel": 399, "churachandpur": 400, "imphal east": 401, "imphal west": 402, "jiribam": 410, "kakching": 413, "kamjong": 409, "kangpokpi": 408, "noney": 412, "pherzawl": 411, "senapati": 403, "tamenglong": 404, "tengnoupal": 407, "thoubal": 405, "ukhrul": 406}, "meghalaya": {"east garo hills": 424, "east jaintia hills": 418, "east khasi hills": 414, "north garo hills": 423, "ri-bhoi": 417, "south garo hills": 421, "south west garo hills": 422, "south west khasi hills": 415, "west garo hills": 420, "west jaintia hills": 416, "west khasi hills": 419}, "mizoram": {"aizawl east": 425, "aizawl west": 426, "champhai": 429, "kolasib": 428, "lawngtlai": 432, "lunglei": 431, "mamit": 427, "serchhip": 430, "siaha": 433}, "nagaland": {"dimapur": 434, "kiphire": 444, "kohima": 441, "longleng": 438, "mokokchung": 437, "mon": 439, "peren": 435, "phek": 443, "tuensang": 440, "wokha": 436, "zunheboto": 442}, "odisha": {"angul": 445, "balangir": 448, "balasore": 447, "bargarh": 472, "bhadrak": 454, "boudh": 468, "cuttack": 457, "deogarh": 473, "dhenkanal": 458, "gajapati": 467, "ganjam": 449, "jagatsinghpur": 459, "jajpur": 460, "jharsuguda": 474, "kalahandi": 464, "kandhamal": 450, "kendrapara": 461, "kendujhar": 455, "khurda": 446, "koraput": 451, "malkangiri": 469, "mayurbhanj": 456, "nabarangpur": 470, "nayagarh": 462, "nuapada": 465, "puri": 463, "rayagada": 471, "sambalpur": 452, "subarnapur": 466, "sundargarh": 453}, "puducherry": {"karaikal": 476, "mahe": 477, "puducherry": 475, "yanam": 478}, "punjab": {"amritsar": 485, "barnala": 483, "bathinda": 493, "faridkot": 499, "fatehgarh sahib": 484, "fazilka": 487, "ferozpur": 480, "gurdaspur": 489, "hoshiarpur": 481, "jalandhar": 492, "kapurthala": 479, "ludhiana": 488, "mansa": 482, "moga": 491, "pathankot": 486, "patiala": 494, "rup nagar": 497, "sangrur": 498, "sas nagar": 496, "sbs nagar": 500, "sri muktsar sahib": 490, "tarn taran": 495}, "rajasthan": {"ajmer": 507, "alwar": 512, "banswara": 519, "baran": 516, "barmer": 528, "bharatpur": 508, "bhilwara": 523, "bikaner": 501, "bundi": 514, "chittorgarh": 521, "churu": 530, "dausa": 511, "dholpur": 524, "dungarpur": 520, "hanumangarh": 517, "jaipur i": 505, "jaipur ii": 506, "jaisalmer": 527, "jalore": 533, "jhalawar": 515, "jhunjhunu": 510, "jodhpur": 502, "karauli": 525, "kota": 503, "nagaur": 532, "pali": 529, "pratapgarh": 522, "rajsamand": 518, "sawai madhopur": 534, "sikar": 513, "sirohi": 531, "sri ganganagar": 509, "tonk": 526, "udaipur": 504}, "sikkim": {"east sikkim": 535, "north sikkim": 537, "south sikkim": 538, "west sikkim": 536}, "tamil nadu": {"aranthangi": 779, "ariyalur": 555, "attur": 578, "chengalpet": 565, "chennai": 571, "cheyyar": 778, "coimbatore": 539, "cuddalore": 547, "dharmapuri": 566, "dindigul": 556, "erode": 563, "kallakurichi": 552, "kanchipuram": 557, "kanyakumari": 544, "karur": 559, "kovilpatti": 780, "krishnagiri": 562, "madurai": 540, "nagapattinam": 576, "namakkal": 558, "nilgiris": 577, "palani": 564, "paramakudi": 573, "perambalur": 570, "poonamallee": 575, "pudukkottai": 546, "ramanathapuram": 567, "ranipet": 781, "salem": 545, "sivaganga": 561, "sivakasi": 580, "tenkasi": 551, "thanjavur": 541, "theni": 569, "thoothukudi (tuticorin)": 554, "tiruchirappalli": 560, "tirunelveli": 548, "tirupattur": 550, "tiruppur": 568, "tiruvallur": 572, "tiruvannamalai": 553, "tiruvarur": 574, "vellore": 543, "viluppuram": 542, "virudhunagar": 549}, "telangana": {"adilabad": 582, "bhadradri kothagudem": 583, "hyderabad": 581, "jagtial": 584, "jangaon": 585, "jayashankar bhupalpally": 586, "jogulamba gadwal": 587, "kamareddy": 588, "karimnagar": 589, "khammam": 590, "kumuram bheem": 591, "mahabubabad": 592, "mahabubnagar": 593, "mancherial": 594, "medak": 595, "medchal": 596, "mulugu": 612, "nagarkurnool": 597, "nalgonda": 598, "narayanpet": 613, "nirmal": 599, "nizamabad": 600, "peddapalli": 601, "rajanna sircilla": 602, "rangareddy": 603, "sangareddy": 604, "siddipet": 605, "suryapet": 606, "vikarabad": 607, "wanaparthy": 608, "warangal(rural)": 609, "warangal(urban)": 610, "yadadri bhuvanagiri": 611}, "tripura": {"dhalai": 614, "gomati": 615, "khowai": 616, "north tripura": 617, "sepahijala": 618, "south tripura": 619, "unakoti": 620, "west tripura": 621}, "uttar pradesh": {"agra": 622, "aligarh": 623, "ambedkar nagar": 625, "amethi": 626, "amroha": 627, "auraiya": 628, "ayodhya": 646, "azamgarh": 629, "badaun": 630, "baghpat": 631, "bahraich": 632, "balarampur": 633, "ballia": 634, "banda": 635, "barabanki": 636, "bareilly": 637, "basti": 638, "bhadohi": 687, "bijnour": 639, "bulandshahr": 640, "chandauli": 641, "chitrakoot": 642, "deoria": 643, "etah": 644, "etawah": 645, "farrukhabad": 647, "fatehpur": 648, "firozabad": 649, "gautam buddha nagar": 650, "ghaziabad": 651, "ghazipur": 652, "gonda": 653, "gorakhpur": 654, "hamirpur": 655, "hapur": 656, "hardoi": 657, "hathras": 658, "jalaun": 659, "jaunpur": 660, "jhansi": 661, "kannauj": 662, "kanpur dehat": 663, "kanpur nagar": 664, "kasganj": 665, "kaushambi": 666, "kushinagar": 667, "lakhimpur kheri": 668, "lalitpur": 669, "lucknow": 670, "maharajganj": 671, "mahoba": 672, "mainpuri": 673, "mathura": 674, "mau": 675, "meerut": 676, "mirzapur": 677, "moradabad": 678, "muzaffarnagar": 679, "pilibhit": 680, "pratapgarh": 682, "prayagraj": 624, "raebareli": 681, "rampur": 683, "saharanpur": 684, "sambhal": 685, "sant kabir nagar": 686, "shahjahanpur": 688, "shamli": 689, "shravasti": 690, "siddharthnagar": 691, "sitapur": 692, "sonbhadra": 693, "sultanpur": 694, "unnao": 695, "varanasi": 696}, "uttarakhand": {"almora": 704, "bageshwar": 707, "chamoli": 699, "champawat": 708, "dehradun": 697, "haridwar": 702, "nainital": 709, "pauri garhwal": 698, "pithoragarh": 706, "rudraprayag": 700, "tehri garhwal": 701, "udham singh nagar": 705, "uttarkashi": 703}, "west bengal": {"alipurduar district": 710, "bankura": 711, "basirhat hd (north 24 parganas)": 712, "birbhum": 713, "bishnupur hd (bankura)": 714, "cooch behar": 715, "coochbehar": 783, "dakshin dinajpur": 716, "darjeeling": 717, "diamond harbor hd (s 24 parganas)": 718, "east bardhaman": 719, "hoogly": 720, "howrah": 721, "jalpaiguri": 722, "jhargram": 723, "kalimpong": 724, "kolkata": 725, "malda": 726, "murshidabad": 727, "nadia": 728, "nandigram hd (east medinipore)": 729, "north 24 parganas": 730, "paschim medinipore": 731, "purba medinipore": 732, "purulia": 733, "rampurhat hd (birbhum)": 734, "south 24 parganas": 735, "uttar dinajpur": 736, "west bardhaman": 737}}
+import os
+import requests,json,time,os
+#from twilio.rest import Client
+
+def get_district_id(state="himachal pradesh",district='hamirpur'):
+  with open('districts.json') as file:
+    dic=json.load(file)
+  state=state.lower()
+  district=district.lower()
+  return dic[state][district]
+
+def data(date,district_id=363):
+      parameters={
+      'district_id':district_id,
+      'date':date
+      }
+
+      r=requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict',params=parameters)
+    #text=json.dumps(r.json(),indent=4)
+      dic=r.json()
+    #print(text)
+      return dic
+#strftime(gmtime())
+
+def get_message(date,district_id,block_name=None,min_age_limit=18):
+    
+    def data(date,district_id=363):
+      parameters={
+      'district_id':district_id,
+      'date':date
+      }
+
+      r=requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict',params=parameters)
+    #text=json.dumps(r.json(),indent=4)
+      dic=r.json()
+    #print(text)
+      return dic
+#strftime(gmtime())
+    
+    def tell(item,vaccine):
+        text=' Hospital: {0}\n Address: {1}\n Block: {3}\n Vaccine: {2} '.format(item['name'],item['address'],vaccine,item['block_name'])
+        #dic=dict({"Hospital":item['name'],"Address":item['address'],'Block':item['block_name']})
+        return text#,dic
+    def vaccine_text(i):
+          text=i['vaccine']+'('+str(i['available_capacity_dose1'])+")"+" Date: {}".format(i['date'])
+          
+          return text
+    available=False
+    vaccine=set()
+    vaccine_dic={}
+    message=''
+    total_vaccine=0
+    dic=data(date,district_id)
+    for item in dic['centers']:
+        for i in item['sessions']:
+            #print(item)
+            if i['min_age_limit']==min_age_limit:
+                  if block_name==None:
+                        if i['available_capacity_dose1']>0:
+                          available=True
+                          text=vaccine_text(i)
+                          vaccine.add(text)
+                          total_vaccine+=1
+                          #dic={i['vaccine']:i['available_capacity_dose1'],'Date':i['date']}
+                  else:
+                        block_name=map(lambda item:item.lower(),block_name)
+                        if i['available_capacity_dose1']>0 and  item['block_name'].lower() in block_name:
+                          available=True
+                          text=vaccine_text(i)
+                          vaccine.add(text)
+                          total_vaccine+=1
+                          #dic={i['vaccine']:i['available_capacity_dose1'],'Date':i['date']}
+                      #tell()
+                      
+                      #print(i,item)
+        if available:
+            text=tell(item,', '.join(vaccine))
+            message+=text+'\n\n'
+            available=False
+            vaccine=set()
+    return message,total_vaccine
+
+
+def send_telegram(message):
+    pass
+    chat_id=os.environ('CHAT_ID')
+    parameter={'chat_id': chat_id,'text':message}
+   
+    token=os.environ['TELEGRAM_TOKEN']
+    
+    requests.get('https://api.telegram.org/bot{}/sendMessage'.format(token),params=parameter)
+
+def send_call(message='hello',heart_beat=1):
+    pass
+    if len(message)==0:
+        return 'not send'
+    account_sid =os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    
+    client = Client(account_sid, auth_token)
+    numbers=list(os.environ['numbers'].split())
+    if heart_beat==1:
+        
+        for number in numbers:
+            call = client.calls.create(
+                                url='http://demo.twilio.com/docs/voice.xml',
+                                to = '+91'+number,
+                                from_='+12017194978'
+                            )
+            # message = client.messages.create(
+            #                         body=message,
+            #                         from_='whatsapp:+14155238886',
+            #                         to='whatsapp:+91'+number
+            #                     )
+            
+    else:
+        pass
+        # message = client.messages.create(
+        #                             body="Bot working well",
+        #                             from_='whatsapp:+14155238886',
+        #                             to='whatsapp:+91'+numbers[0]
+        #                         )
+
+
+def decide(message,total_vaccine):
+    H=time.strftime('%H')
+    M=time.strftime('%M')
+    HM=H+":"+M
+    Sec=time.strftime('%S')
+    send_telegram(message)
+
+    if HM in('07:00','19:00') and int(Sec) in range(5)  :
+          print('BOT beat @ {}:{}:{}'.format(H,M,Sec))
+          
+          #send_telegram("BOT beat @ {}:{}:{}".format(H,M,Sec),0)
+    else:
+        if H in range(6,21) and total_vaccine>=2:
+              send_call(message)
+
+def run(id):
+    os.environ['TZ'] = 'india-05:30'
+    time.tzset()
+    
+    #send_telegram('app started')
+    
+    print('search started')
+
+    while True:
+        date=time.strftime('%d-%m-%Y')
+        
+        message,total_vaccine=get_message(date=date,district_id=id) #optional parameter-->block_name={'haveli','any block near you'})
+        
+        #send_telegram(message)
+        
+        print(message)
+        
+        #decide(message,total_vaccine)
+        
+        time.sleep(50)
+def finder(state="himachal pradesh",district='hamirpur'):
+  id=dic[state][district]
+  print(data(time.strftime('%d-%m-%Y'),district_id=id)
+  print('Data transfer started')
+  run(id)
+
+if __name__ == "__main__":     
+  state=input('Enter Your State here: ')
+  district=input('Enter Your District here: ')
+  
+  id=dic[state][district]
+  print(data(time.strftime('%d-%m-%Y'),district_id=id)
+  print('Data transfer started')
+  run(id)
